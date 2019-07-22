452074c624481ce6e2f57e68f3972bbe7f1a86fe
Marcelo Vieira <marcelosouzav@gmail.com>
2019-07-22 15:14:36 -0300

    teste jenkinsfile

--- src/main/java/com/br/marcelosv/cicd/cicdproject/CICDApplication.java
+++ src/main/java/com/br/marcelosv/cicd/cicdproject/CICDApplication.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -20,6 +21,7 @@ public class CICDApplication {
     @Value("${versao.ambiente}")
     private String versao;
 
+    @ResponseBody
     @GetMapping("/status")
     public String status() {
         return "Olá, você está no ambeinte de " + ambiente + " na versão " + versao;
